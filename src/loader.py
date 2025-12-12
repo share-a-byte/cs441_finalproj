a104cff7164c633b6d7e91377864bde8f1ab47d7
Eero Dunham <eerodunham@gmail.com>
2025-12-12 15:00:02 -0600

    New class structure

--- src/loader.py
+++ src/loader.py
@@ -4,54 +4,32 @@ import torch.nn.functional as F
 import pandas as pd
 from torch.utils.data import DataLoader, Dataset, random_split
 import shutil
+import os
 
-from data_processing import AudioProcessing
+import data_processing.AudioProcessing as AP
 from src.video_clipping import SongDownload
 
 df = pd.read_csv("data/songs.csv")
 
 class SoundDataset(Dataset):
-    def __init__(self, df):
-        self.df = df
-        self.temp_path = "video_clipping/clips/"
-        self.duration = 1000
-        self.sr = 44100
+    def __init__(self, local_path, duration=1000, sr=44100, clip_pool_capacity=80):
+        self.duration = duration
+        self.sr = sr
         self.channel = 2
         self.shift_pct = 0.4
+        self.downloader = SongDownload.SongDownloader(local_path=local_path, capacity=clip_pool_capacity)
+        self.local_path = local_path
 
     def __len__(self):
         return len(self.df)   
 
-    def __getitem__(self, idx):
-        link = self.df.loc[idx, 'LINK']
-        SongDownload.download_script(link)
+    def __getitem__(self):
+        clip = self.downloader.get_clip()
+        aud = AP.Utils.get_audio_and_rechannel(audio_file, self.n_channels)
+    reaud = AP.Utils.resample(aud, self.sr)
+    shift_aud = AP.Utils.time_shift(reaud, self.shift_pct)
+    sgram = AP.Utils.spectrogram(shift_aud)
+    aug_sgram = AP.Utils.augment_spectrogram(sgram, max_mask=0.1, n_fmask=2, n_tmask=2)
+        os.remove(os.path.join(self.local_path, clip))
         # eero's resampling code
         shutil.rmtree(self.temp_path)
-
-class AudioCNN(nn.Module):
-    def __init__(self, num_classes=10, dropout = 0.5):
-        super(AudioCNN, self).__init__()
-        self.classifier = nn.Sequential(
-            nn.Conv2d(in_channels=2, out_channels=16, kernel_size=3, padding=1),
-            nn.BatchNorm2d(16),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-			nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.AdaptiveAvgPool2d((4, 4)),
-            nn.Dropout(dropout),
-            nn.Linear(256 * 4 * 4, 256),
-            nn.Linear(256, num_classes)
-		)
-    def forward(self, x):
-        return self.classifier(x)
-       
