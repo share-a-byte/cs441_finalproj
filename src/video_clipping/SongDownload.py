5f1282569a341d52487ae05eb2ea4fbdb535f79f
VANI RAMESH <vanir2@illinois.edu>
2025-12-13 15:35:59 -0600

    train test val finished in downloader

--- src/video_clipping/SongDownload.py
+++ src/video_clipping/SongDownload.py
@@ -107,7 +107,7 @@ class SongDownloader:
         else:
             self.val_df = self.val_df.drop(sampled.index)
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 #     # set local path to be your current directory
 #         # capacity = 80 -> 80 clips max
 #             # duration = we pull in this size batch (duration = 60 -> 60 second batch)
@@ -115,7 +115,7 @@ class SongDownloader:
 #     # finalized_real = pd.read_csv("FINAL.csv")
 #     # finalized_small = finalized_real[:2]
 #     # finalized_small.to_csv("FINAL.csv") # For testing purposes
-#     downloader = SongDownloader(capacity=10)
+    downloader = SongDownloader(capacity=10)
 #     # length = downloader.get_clips_length()
 
 #     print(downloader.train_df.iloc[0])
@@ -127,6 +127,9 @@ class SongDownloader:
 #     # assert(length == 76.0)
 
 #     # print(result1, result2)
+    
+    downloader.get_clip("train")
+    print(downloader.clip_pool)
 
 #     # os.chdir("src/video_clipping/")
 #     # finalized_real.to_csv("FINAL.csv", index=False)
